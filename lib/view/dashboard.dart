58e36e17457d6b087f994405136bd57119585262
Skh121 <107852103+Skh121@users.noreply.github.com>
2025-05-19 14:51:41 +0545

    Dashboard Screen Modified

--- lib/view/dashboard.dart
+++ lib/view/dashboard.dart
@@ -6,8 +6,26 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
-      body: const Center(child: Text('Welcome to the Dashboard!')),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 12,horizontal: 6),
+              child: Column(
+                children: [
+                  Text("Welcome Back, User"),
+                  Text(
+                    "Dashboard",
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset("assets/images/dashboardImage.png"),
+          ],
+        ),
+      ),
     );
   }
 }

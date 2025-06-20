73bffd02e18b7766ece1d60291981e56f4d29c88
Skh121 <107852103+Skh121@users.noreply.github.com>
2025-06-20 14:37:59 +0545

    Implement AuthLocalDataSource for user authentication operations

---
+++ lib/features/auth/data/data_source/auth_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:tradeverse/features/auth/domain/entity/user_entity.dart';
+
+abstract interface class IAuthDataSource {
+  Future<void> createAccount(UserEntity user);
+  Future<String> loginToAccount(String email,String password);
+}

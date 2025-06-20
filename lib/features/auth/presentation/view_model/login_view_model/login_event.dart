10448b595ca1e9ac34a5f6da93a7a7d9d3d8a694
Skh121 <107852103+Skh121@users.noreply.github.com>
2025-06-20 14:47:06 +0545

    Add LoginView, LoginViewModel, and related events and states for user authentication

---
+++ lib/features/auth/presentation/view_model/login_view_model/login_event.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+abstract class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class LoginEmailChanged extends LoginEvent {
+  final String email;
+
+  const LoginEmailChanged(this.email);
+
+  @override
+  List<Object?> get props => [email];
+}
+
+class LoginPasswordChanged extends LoginEvent {
+  final String password;
+
+  const LoginPasswordChanged(this.password);
+
+  @override
+  List<Object?> get props => [password];
+}
+
+class LoginSubmitted extends LoginEvent {
+  final String email;
+  final String password;
+
+  const LoginSubmitted(this.email, this.password);
+
+  @override
+  List<Object?> get props => [email, password];
+}
+
+class ResetFormStatus extends LoginEvent {}
+
+class NavigateToSignupEvent extends LoginEvent {
+  final BuildContext context;
+  const NavigateToSignupEvent({required this.context});
+}

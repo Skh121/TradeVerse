8e83ee67a2287664c01245e1a712ef3650060acf
Skh121 <107852103+Skh121@users.noreply.github.com>
2025-06-20 14:49:25 +0545

    Remove unused screens and update main entry point to initialize dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:tradeverse/app.dart';
+import 'package:tradeverse/app/app.dart';
+import 'package:tradeverse/app/service_locator/service_locator.dart';
+import 'package:tradeverse/core/network/hive_service.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
+  await HiveService().init();
   runApp(const App());
 }
